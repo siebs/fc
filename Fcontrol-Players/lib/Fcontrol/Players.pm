9747f1440088f6ab6f5292b3281519f1b4d17423
siebs <brettsiebert@yahoo.com>
2014-10-07 21:54:00 -0700

    Adding the Fcontrol::Players module

---
+++ Fcontrol-Players/lib/Fcontrol/Players.pm
@@ -0,0 +1,110 @@
+package Fcontrol::Players;
+
+use 5.016002;
+use strict;
+use warnings;
+
+require Exporter;
+
+our @ISA = qw(Exporter);
+
+# Items to export into callers namespace by default. Note: do not export
+# names by default without a very good reason. Use EXPORT_OK instead.
+# Do not simply export all your public functions/methods/constants.
+
+# This allows declaration	use Fcontrol::Players ':all';
+# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
+# will save memory.
+our %EXPORT_TAGS = ( 'all' => [ qw(
+	
+) ] );
+
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+
+our @EXPORT = qw(
+	
+);
+
+our $VERSION = '0.01';
+
+
+# Preloaded methods go here.
+
+# Constructor. May not be needed, since these functions do not require an
+# object.
+sub new {
+	my ($class) = @_;
+	
+	my $self = {};
+
+	# bless ourself and class
+	bless $self, $class;
+
+	return($self);
+}
+
+
+
+
+my $kungfoo = 'STRONG';
+__END__
+# Below is stub documentation for your module. You'd better edit it!
+
+=head1 NAME
+
+Fcontrol::Players - NFL Player classes and routines
+
+=head1 SYNOPSIS
+
+  use Fcontrol::Players;
+  
+
+=head1 DESCRIPTION
+
+Stub documentation for Fcontrol::Players, created by h2xs. It looks like the
+author of the extension was negligent enough to leave the stub
+unedited.
+
+I left that in on purpose just to irk snotty perl engineers.
+
+So, Blah blah blah.
+
+The subroutines included in this package are:
+ trim
+ ltrim
+ rtrim
+ remWhite      
+ mkXMLTidy    
+ mkPerlTidy
+
+=head2 EXPORT
+
+None by default.
+
+
+
+=head1 SEE ALSO
+
+Mention other useful documentation such as the documentation of
+related modules or operating system documentation (such as man pages
+in UNIX), or any relevant external documentation such as RFCs or
+standards.
+
+If you have a mailing list set up for your module, mention it here.
+
+If you have a web site set up for your module, mention it here.
+
+=head1 AUTHOR
+
+Siebs, brettsiebert@yahoo.com
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2014 by Siebs
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.16.2 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
